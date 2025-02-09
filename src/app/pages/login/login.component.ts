e9695e1374d9ef79d07d01b9cf548555332ab237
Haurio Vieira <hauriovieira@gmail.com>
2025-02-09 00:18:31 +0000

    Adicionando Rodapé

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
 
 @Component({
-  standalone: true,
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss'],
-  imports: [MdbFormsModule],
+  styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {}
+export class LoginComponent {
+  currentYear: number = new Date().getFullYear(); // Define o ano atual
+}
